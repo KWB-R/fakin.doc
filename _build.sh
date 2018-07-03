b0246643fffc1c4271c9e29bb7aa1c1ecc6c72f4
mrustl <michael.rustler@kompetenz-wasser.de>
2018-07-03 15:09:16 +0200

    Call rmarkdown::render_site() for each bookdown output individually (in this case pdf generation locally works fine in case this approach is used but fails in case of rmarkdown::render_site(encoding = 'UTF-8')

--- _build.sh
+++ _build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::epub_book', encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
